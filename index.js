0adb846e8185804367dd579e005e75dfde525d29
Titus Wormer <tituswormer@gmail.com>
2016-11-14 18:51:24 +0100

    Rewrite

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
 'use strict';
 
-/**
- * Dependencies.
- */
+/* Dependencies. */
+var metaphone = require('metaphone');
 
-var metaphone = require('wooorm/metaphone@0.1.4');
-
-/**
- * DOM elements.
- */
-
+/* Nodes. */
 var $input = document.getElementsByTagName('input')[0];
 var $output = document.getElementsByTagName('output')[0];
 
-/**
- * Event handlers.
- */
-
-function oninputchange() {
-    $output.textContent = metaphone($input.value);
-  }
-
-/**
- * Listen.
- */
-
+/* Listen. */
 $input.addEventListener('input', oninputchange);
 
-/**
- * Initial answer.
- */
-
+/* Initial answer. */
 oninputchange();
+
+/* Calculate. */
+function oninputchange() {
+  $output.textContent = metaphone($input.value);
+}
