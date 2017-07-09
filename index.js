849afa7c4047ab33ed0ce035b1d6f75acd792788
Titus Wormer <tituswormer@gmail.com>
2017-07-09 12:15:34 +0200

    Rebuild

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 'use strict';
 
-/* Dependencies. */
 var metaphone = require('metaphone');
 
-/* Nodes. */
 var $input = document.getElementsByTagName('input')[0];
 var $output = document.getElementsByTagName('output')[0];
 
-/* Listen. */
 $input.addEventListener('input', oninputchange);
 
-/* Initial answer. */
 oninputchange();
 
-/* Calculate. */
 function oninputchange() {
   $output.textContent = metaphone($input.value);
 }
