5ac5562197ff79a665e046cee33deeab24adee5c
Titus Wormer <tituswormer@gmail.com>
2018-08-29 20:29:38 +0200

    Update website

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
-var metaphone = require('metaphone');
+var metaphone = require('metaphone')
 
-var $input = document.getElementsByTagName('input')[0];
-var $output = document.getElementsByTagName('output')[0];
+var $input = document.getElementsByTagName('input')[0]
+var $output = document.getElementsByTagName('output')[0]
 
-$input.addEventListener('input', oninputchange);
+$input.addEventListener('input', oninputchange)
 
-oninputchange();
+oninputchange()
 
 function oninputchange() {
-  $output.textContent = metaphone($input.value);
+  $output.textContent = metaphone($input.value)
 }
